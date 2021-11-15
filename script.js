46ed38f5e4412a65b177945e4aa955cd2363ec83
Leim <la.blake107@gmail.com>
2021-11-15 15:51:33 +0100

    Basic functions working, buttons not ready yet

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
+const divContainer = document.querySelector('.container');
+
+let n = prompt("Choose the number of squares per side. (Max. 100)");
+let m = ((600 / n) - 2);
+let i;
+getGrid();
+
 function addSquare() {
     const divContainer = document.querySelector('.container');
+    function addColumn() {
+        divContainer.style.setProperty('grid-template-columns', 'repeat(' + n + ',' + (m + 2 + "px") + ')');
+        divContainer.style.setProperty('grid-template-rows', 'repeat(' + n + ',' + (m + 2 + "px") + ')');
+    }
+    addColumn();
     const square = document.createElement('div');
-    //square.setAttribute('id', 'square');
     square.classList.add('square');
     square.addEventListener("mouseover", mouseOver);
     function mouseOver() {
         square.style.background = "gray";
-      }
+    }
+    square.style.width = m + "px";
+    square.style.height = m + "px";
     divContainer.appendChild(square);
+
+    const restart = document.querySelector('.restart');
+    restart.onclick = () => {
+        remove();
+    }
+
+    function remove() {
+        document.querySelector('.square');
+        square.style.background = "";
+    }
+
 }
 
-for (let n = 0; n < 256; n++) {
-    if (n <= 255) {
-        addSquare();
-    };
+function getGrid() {
+    if (n <= 100) {
+        for (i = 0; i < (n * n); i++) {
+            if (i <= (n * n - 1)) {
+                addSquare();
+            }
+        }
+    }
 }
+
+
+
+const change = document.querySelector('.change');
+change.onclick = () => {
+    
+}
+
+
 
